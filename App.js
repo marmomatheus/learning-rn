93cdbb08d252226f5fd624db30340bf19962bcf2
Matheus Marmo <matheusmarmo55@gmail.com>
2019-04-30 11:47:52 -0300

    gerando apk

--- App.js
+++ App.js
@@ -43,9 +43,17 @@ export default class App extends React.Component {
       const values = [...this.state.values]
       try {
         values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
-      } catch {
-        return 0;
+      } catch(e) {
+        values[0] = this.state.values[0]
       }
+      values[1] = 0
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals, values
+      })
     }
   }
 
